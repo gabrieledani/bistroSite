edac8a155c3e0d8b1b418b355e6c1f422d999b17
Gabriele Dani <gabrieledani@gmail.com>
2021-11-02 11:56:41 -0300

    changed admin area

--- website/admin.py
+++ website/admin.py
@@ -3,4 +3,9 @@ from .models import UserProfile,Reservation
 
 # Register your models here.
 admin.site.register(UserProfile)
-admin.site.register(Reservation)
+#admin.site.register(Reservation)
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+	list_display = ('user_id','id','name')
+	ordering = ('user_id','id')
