f69f9bfc6e651a740aa5ab464fa8a774fc2838cf
Gabriele Dani <gabrieledani@gmail.com>
2021-11-04 17:34:57 -0300

    changed admin area and text

--- website/admin.py
+++ website/admin.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from .models import UserProfile,Reservation
 
 # Register your models here.
-admin.site.register(UserProfile)
+#admin.site.register(UserProfile)
 #admin.site.register(Reservation)
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
 	list_display = ('user_id','id','name')
+	ordering = ('user_id','id')
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('user_id','id','phone_number','paid','can_change')
 	ordering = ('user_id','id')
